f6bb9aa53eb2d58c517e42551f99257df8723b28
Fotis Pegios <fotispeg@gmail.com>
2017-11-26 02:04:37 +0100

    single-page applications part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -55,7 +56,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [ DishService ]
+  providers: [ DishService,
+    PromotionService ]
   ,
   bootstrap: [AppComponent]
 })
